737a8621325ae2d0d4c22248b6af571039d8c046
Eva Janoskova <janoskova.evca@gmail.com>
2021-05-08 12:32:40 +0200

    Upraveno tlacitko pro objednani

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css';
 
 console.log('funguju!');
 
+//3
 const showNavigace = () => {
   navigace.classList.toggle('nav-closed');
 };
@@ -14,3 +15,22 @@ const polozkaNavigace = document.querySelectorAll('.polozka-navigace');
 for (let i = 0; i < polozkaNavigace.length; i++) {
   polozkaNavigace[i].addEventListener('click', showNavigace);
 }
+
+//4
+let ordered = false;
+
+const changeButton = () => {
+  if (ordered === false) {
+    orderBtn.textContent = 'Zrušit';
+    drinkCupElm.classList.add('drink__cup--selected');
+    ordered = true;
+  } else if (ordered === true) {
+    orderBtn.textContent = 'Objednat';
+    drinkCupElm.classList.remove('drink__cup--selected');
+    ordered = false;
+  }
+};
+
+const orderBtn = document.querySelector('.order-btn');
+const drinkCupElm = document.querySelector('.drink__cup');
+orderBtn.addEventListener('click', changeButton);
