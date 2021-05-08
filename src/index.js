0d8c965465a8fe27dad6a2abd98cdf339f0c5b84
Eva Janoskova <janoskova.evca@gmail.com>
2021-05-08 11:59:45 +0200

    Upravena navigace

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './style.css';
 
 console.log('funguju!');
+
+const showNavigace = () => {
+  navigace.classList.toggle('nav-closed');
+};
+
+const navBtn = document.querySelector('#nav-btn');
+const navigace = document.querySelector('nav');
+navBtn.addEventListener('click', showNavigace);
+
+const polozkaNavigace = document.querySelectorAll('.polozka-navigace');
+for (let i = 0; i < polozkaNavigace.length; i++) {
+  polozkaNavigace[i].addEventListener('click', showNavigace);
+}
